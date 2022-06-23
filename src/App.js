28c31c6072a979a30ccad87e5e58cc9176b9d85a
Dwayne Man <dwayne.man@gmail.com>
2022-06-24 01:07:54 +0800

    Slider completed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import {FiChevronRight, FiChevronLeft } from "react-icons/fi"
+import { FaQuoteRight } from 'react-icons/fa'
+import data from "./data"
 
 function App() {
+  const [people, setPeople] = useState(data)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  // currentIndex: less than data length set to lastIndex. Greater than: set to 0
+  useEffect(() => {
+    const lastIndex = people.length - 1 // last index in array 
+    // if currentIndex < 0, set currentIndex to array length-1
+    if(currentIndex < 0){
+      setCurrentIndex(lastIndex)
+    }
+    if(currentIndex > lastIndex) {
+      setCurrentIndex(0)
+    }
+  }, [currentIndex, people])
+
+  // auto run slider using setInterval
+  useEffect(() => {
+    let sliderInterval = setInterval(() => {
+      // increment currentIndex
+      setCurrentIndex(currentIndex + 1)
+    }, 3000)
+
+    // cleanup: remove previous setinterval on next, prev btns
+    return () => {
+      clearInterval(sliderInterval)
+    }
+
+  }, [currentIndex]) // run when currentIndex changes
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+
+      <div className="title">
+        <h2><span>/</span>Reviews</h2>
+      </div>
+
+
+      <div className="section-center">
+        {/* map data into article tag */}
+        {people.map((item, index) => {
+          // destructure person item obj
+          const {id, image, name, title, quote} = item
+          // right side class
+          let position = "nextSlide"
+          // if item index same as state currentIndex then add active class
+          if(index === currentIndex) {
+            position="activeSlide"
+          } 
+          // left side class
+          if(index === currentIndex - 1 || currentIndex === 0 && index === people.length - 1 ) {
+            position = "lastSlide"
+          }
+
+          return(
+            <article key={id} className={position}>
+              <img src={`${image}`} alt={title} className="person-img" />
+                <h2>{name}</h2>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon"/>
+            </article>
+          )
+        })}
+        <button className="prev" onClick={() => setCurrentIndex(currentIndex - 1)}><FiChevronLeft /></button>
+        <button className="next" onClick={() => setCurrentIndex(currentIndex + 1)}><FiChevronRight /></button>
+      </div>
+    </section>
   );
 }
 
